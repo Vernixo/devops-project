6c46fc45d02eed9ecccee95b8fc3ca9cf17f5770
Mateusz Michalik <mateuszmichalikxtr@gmail.com>
2025-01-10 20:45:40 +0100

    Added substraction feature

--- app.py
+++ app.py
@@ -8,12 +8,20 @@ def home():
 
         num1 = request.form.get('num1', default=0, type=float)
         num2 = request.form.get('num2', default=0, type=float)
-        result = num1 + num2
+        operation = request.form.get('operation')
+
+        if operation == 'add':
+            result = num1 + num2
+            operation_symbol = '+'
+        elif operation == 'subtract':
+            result = num1 - num2
+            operation_symbol = '-'
+
         return f"""
-            <h1>Wynik dodawania: {num1} + {num2} = {result}</h1>
+            <h1>Wynik operacji: {num1} {operation_symbol} {num2} = {result}</h1>
             <a href="/">Powrót</a>
         """
-
+    
     return """
         <form method="POST">
             <label for="num1">Liczba 1:</label>
@@ -21,8 +29,14 @@ def home():
             <br>
             <label for="num2">Liczba 2:</label>
             <input type="number" step="any" id="num2" name="num2" required>
+            <br>
+            <label for="operation">Operacja:</label>
+            <select id="operation" name="operation">
+                <option value="add">Dodawanie</option>
+                <option value="subtract">Odejmowanie</option>
+            </select>
             <br><br>
-            <button type="submit">Dodaj</button>
+            <button type="submit">Oblicz</button>
         </form>
     """
 
